de62b38c3b3fa00db3048c9a16359461c69e750e
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-09 07:49:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+  array = (2...integer).to_a
+  if integer <= 1
+    return false
+  elsif integer == 2
+    return true
+  else
+    array.each do |number|
+     if integer % number == 0
+       return false
+       break
+     end
+    end
+    return true
+  end
+end
