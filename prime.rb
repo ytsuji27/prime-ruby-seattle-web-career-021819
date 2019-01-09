19106c1a02539de6ff826420ea71e42a6c9c169e
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-09 07:22:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  else
+    array = (2...integer).to_a
+    array.each do |number|
+     if (integer % number) == 0
+       FALSE
+     else
+       TRUE
+     end
+    end
+  end
+end
